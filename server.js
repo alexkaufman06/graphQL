ca0b40091f2a602d68e9e23325cb3bd112018f7f
Kaufman <alexkaufman06@gmail.com>
2019-06-24 12:56:48 -0700

    Add mutations and input types

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
   type RandomDie {
     numSides: Int!
     rollOnce: Int!
@@ -15,8 +26,23 @@ var schema = buildSchema(`
     songOfTheDay: String
     random: Float!
     getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    update(id: ID!, input: MessageInput): Message
   }
 `);
+
+// If Message had any complex fields, we'd put them on this object
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
 
 // This class implements the RandomDie GraphQL type
 class RandomDie {
@@ -37,8 +63,30 @@ class RandomDie {
   }
 }
 
+// Maps username to content
+var fakeDatabase = {};
+
 // The root provides the top-level API endpoints
 var root = {
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('No message exists with id: ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    var randomId = require('crypto').randomBytes(10).toString('hex');
+    fakeDatabase[randomId] = input;
+    return new Message(randomId, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error ('No message exists with id: ' + id);
+      // This replaces all old data, but some apps might want a partial update.
+      fakeDatabase[id] = input;
+      return new Message(id, input);
+    }
+  },
   quoteOfTheDay: () => {
     return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
@@ -70,6 +118,15 @@ console.log('Running a GraphQL API server at localhost:4000/graphql');
   getDie(numSides: 6) {
     rollOnce
     roll(numRolls: 3)
+  }
+}
+
+mutation {
+  createMessage(input: {
+    author: "andy",
+    content: "hope is a good thing"
+  }) {
+    id
   }
 }
 
