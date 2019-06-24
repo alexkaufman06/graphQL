3dfc1af58de8f2d250d6c61013b7b3fc4cd7fe70
Kaufman <alexkaufman06@gmail.com>
2019-06-24 08:17:57 -0700

    Add object types

--- server.js
+++ server.js
@@ -4,19 +4,52 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    hello: String
-    goodbye: String
+    quoteOfTheDay: String
+    songOfTheDay: String
+    random: Float!
+    getDie(numSides: Int): RandomDie
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+// The root provides the top-level API endpoints
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
-  goodbye: () => {
-    return "Goodbye";
+  songOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Giovanni' : "Stellar"
+  },
+  random: () => {
+    return Math.random();
+  },
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
   }
 };
 
@@ -28,3 +61,36 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+/*     *CALLS FOR GRAPHQL INTERFACE*
+{
+  songOfTheDay
+  quoteOfTheDay
+  random
+  getDie(numSides: 6) {
+    rollOnce
+    roll(numRolls: 3)
+  }
+}
+
+      *THIS CAN BE LOGGED IN CONSOLE*
+var dice = 3;
+var sides = 6;
+var query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+fetch('/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: { dice, sides },
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
+*/
